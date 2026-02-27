ee887c1781cb2d12ce15c81522775df71c72c56b
superquique <enriqueveintidos@gmail.com>
2026-02-27 04:30:14 -0600

    feat: implement player movement

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
-console.log("Hello world!");
+
+
+
+class Player {
+    constructor () {
+        this.positionX = 40;
+        this.positionY = 0;
+        this.width = 20;
+        this.height =  20;
+
+        this.updateUI();
+    }
+
+    updateUI () {
+        const playerElm = document.getElementById("player");
+        playerElm.style.left = this.positionX + "vw";
+        playerElm.style.bottom = this.positionY + "vw";
+        playerElm.style.width = this.width + "vw";
+        playerElm.style.height = this.height + "vh";
+    }
+
+    moveLeft () {
+        if (this.positionX > 0) {
+            this.positionX -= 1;
+            this.updateUI();
+        }
+    }
+
+    moveRight () {
+        if (this.positionX < 100 - this.width) {
+            this.positionX += 1;
+            this.updateUI();
+        }
+    }
+}
+
+const player = new Player();
+
+
+document.addEventListener("keydown", (event) => {
+    if (event.code === "ArrowLeft") {
+        player.moveLeft();
+    } else if (event.code === "ArrowRight") {
+        player.moveRight();
+    }
+})
