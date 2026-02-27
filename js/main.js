20d73d284e7ab09c80558ea0154e32d93c0949ac
superquique <enriqueveintidos@gmail.com>
2026-02-27 06:00:16 -0600

    feat: implement obstacles

--- js/main.js
+++ js/main.js
@@ -3,18 +3,17 @@
 
 class Player {
     constructor () {
-        this.positionX = 40;
-        this.positionY = 0;
         this.width = 20;
         this.height =  20;
-
+        this.positionX = 40;
+        this.positionY = 0;
         this.updateUI();
     }
 
     updateUI () {
         const playerElm = document.getElementById("player");
         playerElm.style.left = this.positionX + "vw";
-        playerElm.style.bottom = this.positionY + "vw";
+        playerElm.style.bottom = this.positionY + "vh";
         playerElm.style.width = this.width + "vw";
         playerElm.style.height = this.height + "vh";
     }
@@ -34,7 +33,58 @@ class Player {
     }
 }
 
+class Obstacle {
+    constructor () {
+        this.width = 20; 
+        this.height = 20;
+        this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
+        this.positionY = 100;
+        this.obstacleElm = null;
+
+        this.createDomElement();
+    }
+
+    createDomElement () {
+        this.obstacleElm = document.createElement("div");
+        this.obstacleElm.className = "obstacle";
+
+        const parentElement = document.getElementById("board");
+        
+        parentElement.appendChild(this.obstacleElm);
+    }
+
+    updateUI () {
+        this.obstacleElm.style.left = this.positionX + "vw";
+        this.obstacleElm.style.bottom = this.positionY + "vh";
+        this.obstacleElm.style.width = this.width + "vw";
+        this.obstacleElm.style.height = this.height + "vh";
+    }
+
+    moveDown () {
+        this.positionY -= 1;
+
+        this.updateUI();
+    }
+}
+
 const player = new Player();
+
+const obstacle1 = new Obstacle();
+
+// generate new obstacles
+
+const obstacleArray = [];
+
+setInterval(() => {
+    const newObstacle = new Obstacle;
+    obstacleArray.push(newObstacle);
+}, 6000);
+
+setInterval(() => {
+    obstacleArray.forEach((obstacleInstance) => {
+        obstacleInstance.moveDown();
+    });
+}, 1000 / 60);
 
 
 document.addEventListener("keydown", (event) => {
